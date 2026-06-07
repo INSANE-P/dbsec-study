613b785cf8d6be2c171318ddf0daacc8ed168342
INSANE-P <chanbin0626@gmail.com>
2026-06-07 23:18:08 +0900

    feat: 페이지 전환 시 스크롤 상단 이동과 인앱 404 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import { ProgressProvider } from "@/lib/progressStore";
+import ScrollToTop from "@/components/ScrollToTop";
 import Layout from "@/components/Layout";
 import HomePage from "@/pages/HomePage";
 import WeekNotesPage from "@/pages/WeekNotesPage";
 import StudyPage from "@/pages/StudyPage";
 import ConceptsPage from "@/pages/ConceptsPage";
 import WrongNotePage from "@/pages/WrongNotePage";
+import NotFoundPage from "@/pages/NotFoundPage";
 
 export default function App() {
   return (
     <ProgressProvider>
+      <ScrollToTop />
       <Routes>
         <Route element={<Layout />}>
           <Route index element={<HomePage />} />
@@ -17,6 +20,7 @@ export default function App() {
           <Route path="study" element={<StudyPage />} />
           <Route path="concepts" element={<ConceptsPage />} />
           <Route path="wrong" element={<WrongNotePage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </ProgressProvider>
